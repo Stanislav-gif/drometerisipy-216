1950fe0651fd040dff17de0d007e12aec22011d7
Станислав-gif <ivanovstas102@gmail.com>
2024-11-12 23:10:35 +0500

    Добавлено сущность DTO обновления заявки

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Annotated
+from typing import Annotated, Optional
 from fastapi import FastAPI, Form
 from pydantic import BaseModel
 
@@ -10,19 +10,25 @@ class Order(BaseModel):
     problemtype: str
     description: str
     client: str
-    status: str  
+    status: str 
+    master: Optional[str] = "Не назначен"
 
+class UpdateOrderDTO(BaseModel):
+    number: int 
+    status: Optional[str] = ""
+    description: Optional[str] = ""
+    master: Optional[str] = ""
 
 repo = [
     Order(
-        number = 1,
-        startDate = "2024-12-05",
-        device = "123",
-        problemtype = "123",
-        description = "123",
-        client = "123",
-        status = "в ожидании"
-    )
+    number = 1,
+    startDate = "2024-12-05",
+    device = "123",
+    problemtype = "123",
+    description = "123",
+    client = "123",
+    status = "в ожидании"
+        )
 ]
 app = FastAPI()
 
