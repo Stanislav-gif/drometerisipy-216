09bfbaf22251548f24f8e8011eab6861af539f90
Станислав-gif <ivanovstas102@gmail.com>
2024-11-24 03:03:13 +0500

    Сделан все пункты 2.5

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ class Order(BaseModel):
     number: int
     startDate: datetime.date
     device: str
-    problemtype: str
+    problemType: str
     description: str
     client: str
     status: str 
     master: Optional[str] = "Не назначен"
-    comments: Optional[str] = []
+    endDate: Optional[datetime.date] = None
+    comments: Optional[list] = []
 
 class UpdateOrderDTO(BaseModel):
     number: int 
@@ -28,7 +29,7 @@ repo = [
     number = 1,
     startDate = "2024-12-05",
     device = "123",
-    problemtype = "123",
+    problemType = "123",
     description = "123",
     client = "123",
     status = "в ожидании"
@@ -37,7 +38,7 @@ repo = [
     number = 2,
     startDate = "2024-12-05",
     device = "123",
-    problemtype = "123",
+    problemType = "123",
     description = "123",
     client = "123",
     status = "в ожидании"
@@ -46,7 +47,7 @@ repo = [
     number = 3,
     startDate = "2024-12-05",
     device = "123",
-    problemtype = "123",
+    problemType = "123",
     description = "123",
     client = "123",
     status = "в ожидании"
@@ -86,6 +87,7 @@ def update_order(dto: Annotated[UpdateOrderDTO , Form()]):
                 message += f"Статус заявки №${o.number} изменен\n"
                 if(o.status == "выполнено"):
                     message += f"Заявки №{o.number} Завершено\n"
+                    o.endDate = datetime.datetime.now()
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
@@ -94,3 +96,32 @@ def update_order(dto: Annotated[UpdateOrderDTO , Form()]):
                 o.comments.append(dto.comment)
             return o
     return "Не найдено"
+
+def complete_count():
+    a = [o for o in repo if o.status == "выполнено"]
+    return len(a)
+
+def get_problem_type_stat():
+    dict = {}
+    for o in repo:
+        if o.problemType in dict.keys():
+            dict[o.problemType] += 1
+        else:
+            dict[o.problemType] = 1
+        return dict
+
+def get_average_time_to_complete():
+    times = [(
+        datetime.datetime(o.endDate.year,o.endDate.month,o.endDate.day) -
+        datetime.datetime(o.startDate.year,o.startDate.month,o.startDate.day)).days
+                 for o in repo 
+                 if o.status == "выполнено"]
+    if complete_count() != 0:
+        return sum(times) / complete_count()
+    return 0
+@app.get("/statistics")
+def get_statistics():
+    return {
+        "complete_count": complete_count(),
+        "problem_type_stat": get_problem_type_stat(),
+        "get_average_time_to_complete": get_average_time_to_complete}
